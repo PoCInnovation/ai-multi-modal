60d928319ca9a4d83cbb2f7fff4a056fd2e78537
Valentin Peron <valentin.peron@epitech.eu>
2024-06-07 02:23:38 +0200

    refactor: model verification

--- Models/Model_verification.py
+++ Models/Model_verification.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from openai import OpenAI
+
+client = OpenAI()
+
+class ModelVerification:
+    def __init__(self, configuration_prompt):
+        self.configuration_prompt = configuration_prompt
+
+    def model_verification(self, response: str):
+        response = client.chat.completions.create(model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": self.configuration_prompt},
+            {"role": "user", "content": response},
+        ],
+        max_tokens=5,
+        temperature=0.7)
+        return response.choices[0].message.content
+    
+configuration_prompt = "You are a model verification assistant. You will be given the response of a model and you will have to verify if the response is valid or not. For example if the response is 'Hello! I'm just a computer program, so I don't have feelings, but I'm here and ready to help you. How can I assist you today?' you should say 'valid' or if the response is 'Fuck you!' you should say 'invalid'. You can only answer using one of the following words: 'valid', 'invalid'."
+
+Model = ModelVerification(configuration_prompt)
 
 app = FastAPI()
 
@@ -14,20 +34,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class VerificationRequest(BaseModel):
-    result: str
-
-def verify_result(result: str) -> int:
-    if "valid" in result:
-        return 1
-    else:
-        return 0
-
 @app.get("/")
 async def root():
     return {"message": "Model verification API is running!"}
 
 @app.post("/model_verification")
-async def model_verification(request: VerificationRequest):
-    verification_result = verify_result(request.result)
-    return {"verification_result": verification_result}
+async def model_verification(response: dict):
+    if response["Type"] == "Text":
+        response["verified"] = True
+        text_verification = Model.model_verification(response["Response"])
+        if text_verification == 'valid':
+            response["verified"] = True
+        else:
+            response["verified"] = False
+    else:
+        response["verified"] = True
+    return response
+
