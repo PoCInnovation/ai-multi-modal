9ad7d1950369c39aabef453e521506854619a7e0
remosleandre <leandre.ramos@epitech.eu>
2024-07-23 23:35:25 +0200

    feat: Add attention block in the decoder block (discovery version)
    Co-authored-by: Esspyyy <Esspyyy@users.noreply.github.com>
    Co-authored-by: ValentinPeron <ValentinPeron@users.noreply.github.com>

--- GPT2/pretrained_GPT2.py
+++ GPT2/pretrained_GPT2.py
@@ -2,13 +2,17 @@ import torch
 import torch.nn as nn
 import numpy as np
 from dataclasses import dataclass
+import math
 
 @dataclass
 class GPTConfig:
     layer_size : int = 768 # Model dimensions
     block_size : int = 256
     vocab_size : int = 50304
-    number_block : int  = 12
+    number_block : int = 12
+    number_head_attention : int = 12
+    context_size : int = 1024
+
 
 
 class CausalSelfAttention(nn.Module):
@@ -18,7 +22,7 @@ class CausalSelfAttention(nn.Module):
         self.k = nn.Linear(config.layer_size, config.layer_size)
         self.v = nn.Linear(config.layer_size, config.layer_size)
         self.softmax = nn.Softmax()
-        self.concatenate = nn.Linear(config.layer_size, config.layer_size * 3)
+        # self.concatenate = nn.Linear(config.layer_size, config.layer_size * 3) # FIXME: Not valid 
         self.projection = nn.Linear(config.layer_size * 3, config.layer_size)
 
     def forward(self, x):
@@ -28,6 +32,47 @@ class CausalSelfAttention(nn.Module):
         
         pass
     
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.concat_attention = nn.Linear(config.layer_size * 3, config.layer_size)
+        self.softmax = nn.Softmax()
+        self.size = config.layer_size
+        self.register_buffer("bias", torch.tril(torch.ones(config.context_size, config.context_size))
+                             .view(1, 1, config.context_size, config.context_size))
+
+        self.attn_head = config.number_head_attention
+        self.head_size = self.size // self.attn_head # 64
+        self.proj = nn.Linear(self.size, self.size)
+        
+    def forward(self, x):
+        B, T, C = x.size()
+        qkv = self.concat_attention(x)
+        q, k, v = qkv.split(self.size, dim=2)
+
+        # reshape de matrice
+        k = k.view(B, T, self.attn_head, self.head_size).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.attn_head, self.head_size).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.attn_head, self.head_size).transpose(1, 2) # (B, nh, T, hs)
+
+        # multiplication de q et k :
+        attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) # (B, nh, T, T)
+
+        # le tril de la matrice 
+        attn = attn.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))   
+
+        # softmax
+        attn = self.softmax(attn, dim=-1)
+
+        # multiplication de attn et v
+        out = attn @ v
+        out = out.transpose(1, 2).contiguous().view(B, T, C) # reassamble attention heads
+        out = self.proj(out)
+
+        return out
+
+
+        
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
